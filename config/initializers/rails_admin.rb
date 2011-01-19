85c3760812c52ba4c7589f74a10b5e3483fd84f6
Grycek <adam.grycner@wp.pl>
2011-01-19 18:41:01 +0100

    Rails admin

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,12 @@
+require "rails_admin/application_controller"
+
+module RailsAdmin
+  class ApplicationController < ::ApplicationController
+    before_filter :authenticate_user!
+    before_filter :authenticate_admin
+    
+    def authenticate_admin
+        redirect_to root_path unless current_user and current_user.is_admin
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110118214634) do
+ActiveRecord::Schema.define(:version => 20110118220017) do
 
   create_table "broadcasts", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(:version => 20110118214634) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "histories", :force => true do |t|
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
