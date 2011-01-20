d6d4d0fdb3015bc3fbddc887c3e88326055e9767
Grycek <adam.grycner@wp.pl>
2011-01-20 21:20:50 +0100

    Change broadcast view

--- app/helpers/broadcasts_helper.rb
+++ app/helpers/broadcasts_helper.rb
@@ -1,6 +1,12 @@
+require 'date'
 module BroadcastsHelper
   def authenticate_presenter
     return false unless current_user and (current_user.broadcasts.include? Broadcast.find(params[:id]) )
     return true
+  end
+  
+  def number_to_day_name(number)
+     w = Date.new(1,1,3 + number)
+     return w.strftime('%A')
   end
 end
