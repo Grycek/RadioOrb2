db8a3212f5545e6feeb72cb5726a4f9cf0ec3425
Grycek <adam.grycner@wp.pl>
2011-01-22 11:09:45 +0100

    Author or presenter authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
     before_filter :authenticate_user!
+    before_filter :authenticate_presenter_or_author, :except => [:create]
     
     
   def create
@@ -29,7 +30,7 @@ class CommentsController < ApplicationController
     @broadcast          = Broadcast.find(params[:broadcast_id])
     @broadcast_playlist = BroadcastPlaylist.find(params[:broadcast_playlist_id])
     @comment            = Comment.find(params[:id])
-    if authenticate_user! and @comment.update_attributes(params[:comment])
+    if authenticate_presenter_or_author and @comment.update_attributes(params[:comment])
       flash[:notice] = "Successfully edit comment."
       redirect_to broadcast_broadcast_playlist_path(@broadcast, @broadcast_playlist)
     else
@@ -42,10 +43,21 @@ class CommentsController < ApplicationController
     @broadcast          = Broadcast.find(params[:broadcast_id])
     @broadcast_playlist = BroadcastPlaylist.find(params[:broadcast_playlist_id])
     @comment            = Comment.find(params[:id])
-    if authenticate_user!
+    if authenticate_presenter_or_author
       @comment.destroy
       flash[:notice] = "Successfully destroyed broadcast playlist."
     end
     redirect_to broadcast_broadcast_playlist_path(@broadcast, @broadcast_playlist)
+  end
+
+
+  
+  def authenticate_presenter_or_author
+    unless (current_user and (current_user.broadcasts.include? Broadcast.find(params[:broadcast_id]) )) or (current_user and (current_user.comments.include? Comment.find(params[:id]) ))
+      flash[:error] = "Permission denied"
+      redirect_to broadcast_broadcast_playlist_url(params[:broadcast_id], params[:broadcast_playlist_id]) and return false 
+    else
+      return true
+    end
   end
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,17 @@
 module CommentsHelper
+
+
+  def authenticate_presenter_or_author(broadcast = nil, comment = nil)
+    broadcast = params[:broadcast_id] if broadcast == nil
+    if comment == nil
+      if params[:comment_id] == nil
+        comment   = params[:id] 
+      else
+        comment   = params[:comment_id]
+      end
+    end
+    return false unless current_user and (current_user and (current_user.broadcasts.include? Broadcast.find( broadcast) )) or (current_user and (current_user.comments.include? Comment.find( comment ) ))
+    return true
+  end
+  
 end
